ea0af8b633af1fe361e703cd07338bd0eb8f11af
Marton Viktor Melar <melar.pr9.2023@uns.ac.rs>
2026-08-06 15:12:15 +0200

    Main

---
+++ main.py
@@ -0,0 +1,14 @@
+#we need the statrting genreations and the main algorithm
+#1. Population initialization
+
+#2. Fitness evaluation --> checking restrictions
+#3. Evolution operators --> selection --> crossover and mutation 
+
+#4. Replace population --> step 2
+
+#Define
+#Population
+#Iteration
+#Eta
+
+
